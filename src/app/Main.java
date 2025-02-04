628109418d91cc99041d3d62e40005c9437914f3
Lenovo <mkaminskyi97@gmail.com>
2025-02-04 20:11:14 +0200

    Update info app

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,7 @@ package app;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("My SSH app.");
+        System.out.println("SHH app for User.");
 
     }
 }
